846fa96650f264effc7073c8e3178eef9e4fcb82
takeover-da <https://github.com/takeover-da>
2024-10-14 17:13:56 +0900

    10.14

---
+++ src/main/groovy/com/example/miniproject/MiniProjectApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.miniproject
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MiniProjectApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MiniProjectApplication, args)
+    }
+
+}
